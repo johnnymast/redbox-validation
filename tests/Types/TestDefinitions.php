7754cdc544142c186e5cdeb27b8a926c11414089
Johnny Mast <mastjohnny@gmail.com>
2023-03-26 01:24:13 +0100

    Updated tge PhpCS action.

--- tests/Types/TestDefinitions.php
+++ tests/Types/TestDefinitions.php
@@ -1,14 +1,16 @@
 <?php
 
-/*
+/**
  * This file is part of the Redbox-validator package.
  *
- * (c) Johnny Mast <mastjohnny@gmail.com
+ * (c) Johnny Mast <mastjohnny@gmail.com>
  *
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
+declare(strict_types=1);
 
 namespace Redbox\Validation\Tests\Types;
 
