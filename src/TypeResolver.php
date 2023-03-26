8ab096cbeade4bcba475fb98551283111a5a00f7
Johnny Mast <mastjohnny@gmail.com>
2023-03-26 01:28:11 +0100

    Updated tge PhpCS action.

--- src/TypeResolver.php
+++ src/TypeResolver.php
@@ -3,15 +3,6 @@
 /*
  * This file is part of the Redbox-validator package.
  *
- * (c) Johnny Mast <mastjohnny@gmail.com
- *
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-/*
- * This file is part of the Redbox-validator package.
- *
  * (c) Johnny Mast <mastjohnny@gmail.com>
  *
  *
@@ -53,7 +44,7 @@ class TypeResolver
 
         return $callables;
     }
-    
+
     /**
      * Read the validation types from a class.
      *
