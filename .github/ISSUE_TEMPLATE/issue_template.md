7c573f34c33334404f89ff98fa5c7619fe0d47fe
Johnny Mast <mastjohnny@gmail.com>
2023-03-30 08:30:00 +0200

    Added a feature request template

---
+++ .github/ISSUE_TEMPLATE/issue_template.md
@@ -0,0 +1,7 @@
+---
+name: Feature request
+about: Request a few feature for Redbox Validator
+title: "[DATE]: [FEATURE NAME]"
+labels: enhancement, feature request
+assignees: johnnymast
+---
---
+++ examples/bug.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+require __DIR__ . '/autoload.php';
+
+use Redbox\Validation\Validator;
+
+$data = ['name' => 'blabla'];
+$validator = new Validator($data);
+
+
+$validator->validate(
+    [
+        'name' => 'required|email|unique:users',
+//                    'email' => 'required|email|unique:users',
+//                    'password' => 'required|min:6|confirmed',
+    ]
+);
+
+
+//            return redirect('https://www.google.com');
+if ($validator->fails()) {
+//                throw new ValidationException($validator->errors());
+
+}
+
+echo "DONE";
